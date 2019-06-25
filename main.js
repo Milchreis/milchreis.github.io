7020fbde271c8425fb964f7e67d6735e49657bc3
Milchreis <nick-m@gmx.de>
2019-06-25 18:20:26 +0200

    initial import

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('#pagepiling').pagepiling({
+        sectionsColor: ['white', '#ee005a', '#2C3E50', '#39C', '#ee005a', '#2C3E50', '#39C'],
+        navigation: {
+            'position': 'right',
+            'tooltips': ['Welcome', 'Phobox', 'Image processing', 
+                         'Zombie-Hunter', 'Tiny-Defence 2', 'Flashy', 'JCopy']
+        }
+    });
+});
