f940fa7d8ba80b5980bc157cb989e9e37f37fca9
Milchreis <nick-m@gmx.de>
2019-06-25 19:20:54 +0200

    Adds some content

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('#pagepiling').pagepiling({
-        sectionsColor: ['white', '#ee005a', '#2C3E50', '#39C', '#ee005a', '#2C3E50', '#39C'],
+        sectionsColor: ['white', '#ee005a', '#4587a9', '#2C3E50', '#ee005a', '#2C3E50', '#39C'],
         navigation: {
             'position': 'right',
             'tooltips': ['Welcome', 'Phobox', 'Image processing', 
